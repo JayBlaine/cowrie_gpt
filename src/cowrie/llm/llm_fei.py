5197e510e1e90d4b8cadd242f5d024671293c867
jragsdale <jbrags42@gmail.com>
2023-12-12 13:44:55 -0600

    fix timeout, filter, stderr combine with stdout

--- src/cowrie/llm/llm_fei.py
+++ src/cowrie/llm/llm_fei.py
@@ -205,16 +205,20 @@ class LlmFEI():
         :param messages:
         :return:
         """
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            temperature=0.00,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0.0,
-        )
+        try:
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=messages,
+                temperature=0.00,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0.0,
+                request_timeout=120
+            )
+            cmd_resp = response["choices"][0]["message"]["content"]
+        except openai.error.Timeout:
+            cmd_resp = ""
 
-        cmd_resp = response["choices"][0]["message"]["content"]
         self.SESSION_TOKENS += len(self.encoding.encode(cmd_resp))
         return self.sanitize_output(cmd_resp)
 
